94f98bf88fc23cd5f1aaf51b10cd3e037f45ef1b
LauritsSchou <laurits.schou@gmail.com>
2023-09-19 10:53:54 +0200

    changed database to use env

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  database: "musicbase_db",
-  password: "Anga0001kea",
+  host: process.env.HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
   multipleStatements: true,
 });
 
