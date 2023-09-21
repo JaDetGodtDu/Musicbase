05da365567d8b6ac868dd9d6e554de5e9163b161
LauritsSchou <laurits.schou@gmail.com>
2023-09-21 15:21:53 +0200

    artist route working with mysql promise wrapper

--- routes/artists.js
+++ routes/artists.js
@@ -3,18 +3,12 @@ import connection from "../database.js";
 
 const artistRouter = Router();
 
-artistRouter.get("/", (req, res) => {
+artistRouter.get("/", async (req, res) => {
   const query = /* SQL */ `SELECT * FROM artists ORDER BY artist_name;`;
-  connection.query(query, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-      res.json({ message: error });
-    } else {
-      res.json(results);
-    }
-  });
+  const [results] = await connection.execute(query);
+  res.json(results);
 });
-artistRouter.get("/search", (request, response) => {
+artistRouter.get("/search", async (request, response) => {
   const query = request.query.q.toLocaleLowerCase();
   const queryString = /*sql*/ `
     SELECT * 
@@ -22,101 +16,43 @@ artistRouter.get("/search", (request, response) => {
     WHERE artist_name LIKE ?
     ORDER BY artist_name`;
   const values = [`%${query}%`];
-  connection.query(queryString, values, (error, results) => {
-    if (error) {
-      console.log(error);
-      response.json({ message: error });
-    } else {
-      response.json(results);
-    }
-  });
+  const [results] = await connection.execute(queryString, values);
+  response.json(results);
 });
 
-artistRouter.get("/:id", (req, res) => {
+artistRouter.get("/:id", async (req, res) => {
   const id = req.params.id;
   const query = /* SQL */ `SELECT * FROM artists WHERE artist_id=?;`;
   const values = [id];
 
-  connection.query(query, values, (error, results) => {
-    if (error) {
-      console.log(error);
-      res.json({ message: error });
-    } else {
-      res.json(results[0]);
-    }
-  });
-});
-artistRouter.get("/:id/albums", (request, response) => {
-  const id = request.params.id;
-
-  const queryString = /*sql*/ `
-        SELECT DISTINCT albums.album_id AS albumId, 
-                        albums.album_name AS albumName,
-                        albums.year_of_release AS releaseDate,
-                        artists.artist_id AS artistId,
-                        artists.artist_name AS artistName
-        FROM albums
-        INNER JOIN albums_tracks ON albums.album_id = albums_tracks.album_id
-        INNER JOIN tracks ON albums_tracks.track_id = tracks.track_id
-        INNER JOIN artists ON albums.artist_id = artists.artist_id
-        INNER JOIN tracks_artists ON tracks.track_id = tracks_artists.track_id
-        WHERE artists.artist_id = ?;
-    `;
-
-  const values = [id];
-
-  connection.query(queryString, values, (error, results) => {
-    if (error) {
-      console.log(error);
-      response.json({ message: error });
-    } else {
-      response.json(results);
-    }
-  });
+  const [results] = await connection.execute(query, values);
+  res.json(results);
 });
 
-artistRouter.post("/", (req, res) => {
+artistRouter.post("/", async (req, res) => {
   const artist = req.body;
   const query = /* SQL */ `INSERT INTO artists(artist_name) values (?);`;
   const values = [artist.artist_name];
 
-  connection.query(query, values, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-      res.json({ message: error });
-    } else {
-      res.json(results);
-    }
-  });
+  const [results] = await connection.execute(query, values);
+  res.json(results);
 });
 
-artistRouter.put("/:id", (req, res) => {
+artistRouter.put("/:id", async (req, res) => {
   const id = req.params.id;
   const artist = req.body;
   const query = /* SQL */ `UPDATE artists SET artist_name=? WHERE artist_id=?`;
   const values = [artist.artist_name, id];
-  connection.query(query, values, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-      res.json({ message: error });
-    } else {
-      res.json(results);
-    }
-  });
+  const [results] = await connection.execute(query, values);
+  res.json(results);
 });
 
 artistRouter.delete("/:id", async (req, res) => {
   const id = req.params.id;
   const query = /* SQL */ `DELETE FROM artists WHERE artist_id=?`;
   const values = [id];
-  connection.query(query, values, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-      res.json({ message: error });
-    } else {
-      res.json(results);
-    }
-  });
+  const [results] = await connection.execute(query, values);
+  res.json(results);
 });
 
 export default artistRouter;
